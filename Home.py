0e91cab081b578c0d00b2cd9d64f4f47bed39d9f
Piotr Kafel <peterkaf132@gmail.com>
2023-06-16 15:47:13 +0200

    Added basic layout, sample json and read json function

--- Home.py
+++ Home.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
+import functions as fun
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    data = fun.read_json()
+    st.title("Todo App")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        entry_box = st.text_input(label="**Enter the activity you want to add:**",
+                                  placeholder="Enter the activity you want to add:",
+                                  label_visibility="hidden")
+    with col2:
+        st.markdown("**Select the priority:**")
+        for entry in data:
+            st.checkbox(entry["priority"])
+    with col3:
+        st.markdown("**Pick a deadline:**")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- functions.py
+++ functions.py
@@ -1,18 +1,22 @@
 """
 This file contains auxiliary functions
 """
+import json
+FILENAME = "todos.json"
 
 
-def read_json(filename):
+def read_json(filename=FILENAME):
     """
     Initiates json read funcitons,
     :param filename: relative path to the file
     :return: List of dictionaries
     """
-    pass
+    with open("todos.json", 'r') as file:
+        content = file.read()
+    return json.loads(content)
 
 
-def write_json(filename, value):
+def write_json(value, filename=FILENAME):
     """
     Saves specified string into json file
     :param filename: relative path to the file
