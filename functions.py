5e55285c43d1c2564453d4b3de835a1087e81c05
Piotr Kafel <peterkaf132@gmail.com>
2023-06-16 14:30:50 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+"""
+This file contains auxiliary functions
+"""
+
+
+def read_json(filename):
+    """
+    Initiates json read funcitons,
+    :param filename: relative path to the file
+    :return: List of dictionaries
+    """
+    pass
+
+
+def write_json(filename, value):
+    """
+    Saves specified string into json file
+    :param filename: relative path to the file
+    :param value: string of an entry to be saved in json
+    :return: None
+    """
+    pass
+
