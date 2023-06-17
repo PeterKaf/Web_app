d0e5ca013ead1dac5af59ac660f002597be8ac87
Piotr Kafel <peterkaf132@gmail.com>
2023-06-17 14:10:57 +0200

    Fixed page layout, added date selector and sorted Todos list. Changed json priority from string to int.

--- Home.py
+++ Home.py
@@ -4,18 +4,26 @@ import functions as fun
 
 def main():
     data = fun.read_json()
+    sorted_todos = sorted(data, key=lambda x: (x['due_date'], x['priority']))
+    priorities = {"High": False, "Medium": False, "Low": False}
     st.title("Todo App")
     col1, col2, col3 = st.columns(3)
     with col1:
-        entry_box = st.text_input(label="**Enter the activity you want to add:**",
-                                  placeholder="Enter the activity you want to add:",
-                                  label_visibility="hidden")
-    with col2:
         st.markdown("**Select the priority:**")
-        for entry in data:
-            st.checkbox(entry["priority"])
-    with col3:
+        for priority, selected in priorities.items():
+            priorities[priority] = st.checkbox(priority, selected)
+        if sum(priorities.values()) > 1:
+            st.warning("Please select only one option")
+    with col2:
         st.markdown("**Pick a deadline:**")
+        selected_date = st.date_input("Select a date")  # YYYY-MM-DD
+    with col3:
+        st.markdown("**Submit a Todo:**")
+        entry_box = st.text_input(label="**Enter the activity you want to add:**",
+                                  placeholder="Enter here...")
+    st.subheader("Currnet Todos:")
+    for todo in sorted_todos:
+        st.checkbox(todo["task"])
 
 
 if __name__ == '__main__':
